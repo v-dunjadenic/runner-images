18211971deb39fcbfb8a833a9c1ada0c8832340a
v-dunjadenic <dunja.denic@akvelon.com>
2026-07-17 10:31:13 +0000

    Attempt to reproduce bug #14162

---
+++ .github/repro/appx-dependency/src/ReproWpfApp/App.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows;
+
+namespace ReproWpfApp
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ .github/repro/appx-dependency/src/ReproWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace ReproWpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
